6fb8070da3b878652799eff5a97c798cfe28c5d5
kimyoungbin1114 <adogen1114@gmail.com>
2026-07-16 08:19:47 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-// 캐시 완전 비활성화 - 항상 최신 버전 사용
-self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
@@ -7,5 +6,5 @@ self.addEventListener('activate', e => {
   );
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => new Response('오프라인 상태입니다')));
+  e.respondWith(fetch(e.request));
 });
